1d62cbcb6a2064595069c5846992f5bc24ee31c8
Nagiur Rahaman <nagiur@Nagiurs-MacBook-Air.local>
2023-06-16 18:57:31 +0800

    added assist.sh and week_03

---
+++ assist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create directories for 7 days
+for i in {1..7}; do
+  rm -rf day_$i && mkdir day_$i && touch day_$i/in.txt day_$i/out.txt
+  echo "in" > day_$i/in.txt
+  echo "out" > day_$i/out.txt
+done
